a6887d01f3d2da22abb857e898e0e73b30f4b28d
Sherif Awad <sherif.hero1@gmail.com>
2024-04-10 15:25:00 -0700

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,3 +2,45 @@
 * File: 1-binary_tree_insert_left.c
 * Author: Sherif Awad
 */
+/* Include the header file for binary trees */
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - Function to insert a new node as the left-child of
+ *				a node in a binary tree
+ * @parent: pointer to a parent node of type binary_tree_t. binary_tree_t
+ * @value: the value that will be stored in the new node
+ *
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	/* Declare a new node of type binary_tree_t */
+	binary_tree_t *LeftNode;
+
+	/* If the parent node is NULL, return NULL */
+	if (parent == NULL)
+		return (NULL);
+
+	/* Create a new node with the given value and parent */
+	LeftNode = binary_tree_node(parent, value);
+
+	/* If memory allocation fails, return NULL */
+	if (LeftNode == NULL)
+		return (NULL);
+
+	/* If the parent node already has a left child */
+	if (parent->left != NULL)
+	{
+		/* Set left child of new node to the current left child of the parent */
+		LeftNode->left = parent->left;
+
+		/* Set the parent of the current left child of the parent to the new node */
+		parent->left->parent = LeftNode;
+	}
+
+	/* Set the left child of the parent to the new node */
+	parent->left = LeftNode;
+
+	/* Return the new node */
+	return (LeftNode);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,8 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 /* task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+/* task 1 */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 
 
