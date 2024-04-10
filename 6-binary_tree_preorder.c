74490928be6ab211b861bb7330216182ded0b642
Sherif Awad <sherif.hero1@gmail.com>
2024-04-10 16:06:45 -0700

    task 6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,3 +2,26 @@
 * File: 6-binary_tree_preorder.c
 * Author: Sherif Awad
 */
+/* Include the header file for binary trees */
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - Function to traverse a binary tree in preorder
+ * @tree: pointer to the root node of the binary tree that we’re traversing
+ * @func: function pointer points to function that takes
+ *			int input and returns nothing
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* If the tree and function pointer are not NULL */
+	if (tree && func)
+	{
+		/* Call the function on the value of the current node */
+		func(tree->n);
+
+		/* Recursively traverse the left subtree in preorder */
+		binary_tree_preorder(tree->left, func);
+
+		/* Recursively traverse the right subtree in preorder */
+		binary_tree_preorder(tree->right, func);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -39,8 +39,8 @@ void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 /* task 5 */
 int binary_tree_is_root(const binary_tree_t *node);
-/* task  */
-
+/* task 6 */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /* task  */
 
 /* task  */
