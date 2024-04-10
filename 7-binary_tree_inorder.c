e8acb567059ec9dadcecc2f72e83dc3ed47b187f
Sherif Awad <sherif.hero1@gmail.com>
2024-04-10 16:11:24 -0700

    task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,3 +2,27 @@
 * File: 7-binary_tree_inorder.c
 * Author: Sherif Awad
 */
+/* Include the header file for binary trees */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Function to traverse a binary tree in inorder
+ * @tree: a pointer to the root node of the binary tree that you’re traversing
+ * @func: function pointer points to function that takes
+ *			int input and returns nothing
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	/* If the tree and function pointer are not NULL */
+	if (tree && func)
+	{
+		/* Recursively traverse the left subtree in inorder */
+		binary_tree_inorder(tree->left, func);
+
+		/* Call the function on the value of the current node */
+		func(tree->n);
+
+		/* Recursively traverse the right subtree in inorder */
+		binary_tree_inorder(tree->right, func);
+	}
+}
--- binary_trees.h
+++ binary_trees.h
@@ -41,8 +41,8 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 /* task 6 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-/* task  */
-
+/* task 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 /* task  */
 
 /* task  */
