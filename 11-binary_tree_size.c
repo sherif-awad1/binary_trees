92a76cee9e31054bc3af7d915b90744e00a6bb85
Sherif Awad <sherif.hero1@gmail.com>
2024-04-10 16:46:33 -0700

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,3 +2,32 @@
 * File: 11-binary_tree_size.c
 * Author: Sherif Awad
 */
+/* Include the header file for binary trees */
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Function to calculate the size of a binary tree
+ * @tree: pointer to the root node of the binary tree that we’re working with
+ * Return: size and if tree is NULL, function must return 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	/* Initialize the size of the tree to 0 */
+	size_t size = 0;
+
+	/* If the tree is not NULL */
+	if (tree)
+	{
+		/* Increment the size for the current node */
+		size += 1;
+
+		/* Recursively add the size of the left subtree */
+		size += binary_tree_size(tree->left);
+
+		/* Recursively add the size of the right subtree */
+		size += binary_tree_size(tree->right);
+	}
+
+	/* Return the size of the tree */
+	return (size);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -50,7 +50,7 @@ size_t binary_tree_height(const binary_tree_t *tree);
 /* task 10 */
 size_t binary_tree_depth(const binary_tree_t *tree);
 /* task 11 */
-
+size_t binary_tree_size(const binary_tree_t *tree);
 /* task 12 */
 
 /* task 13 */
