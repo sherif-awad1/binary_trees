cc6601d14f4bb39c8866b37f85b17a882209fbbb
Sherif Awad <sherif.hero1@gmail.com>
2024-04-10 18:17:36 -0700

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,23 +27,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  * @tree: pointer to the root node of the binary tree that we’re working with
  * Return: haight and if tree is NULL, function must return 0
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/* If the tree is not NULL */
 	if (tree)
 	{
-		/* Declare variables to store the heights of the left and right subtrees */
 		size_t l = 0, r = 0;
-		/* Calculate height of left subtree if it exists, otherwise set it to 0 */
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 
-		/* Calculate height of right subtree if it exists, otherwise set it to 0 */
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		/* Return the greater of the two heights */
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
-
-	/* If the tree is NULL, return 0 */
 	return (0);
 }
